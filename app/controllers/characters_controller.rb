b7bcee7000c3eaddc8b86cec1d935b829f0c3c04
Will VanderWyden <vanderwyden@gmail.com>
2025-03-05 18:08:36 +0900

    initial commit

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -13,4 +13,25 @@ class CharactersController < ApplicationController
     @character = Character.new
   end
 
+  def create
+    @character = Character.new(character_params)
+    if @character.save
+      redirect_to character_path(@character)
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @character = Character.find(params[:id])
+    @character.destroy
+    redirect_to characters_path, status: :see_other
+  end
+
+  private
+
+  def character_params
+    params.require(:character).permit(:name, :age, :job, :race, :gender)
+  end
+
 end
