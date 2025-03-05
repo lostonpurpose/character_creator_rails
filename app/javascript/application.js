d789d1b5bbd8f0986ba257699074fe7a59e2a513
Will VanderWyden <vanderwyden@gmail.com>
2025-03-05 18:41:06 +0900

    dice random math working

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,19 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+
+let d1 = document.querySelector(".d1");
+let d2 = document.querySelector(".d2");
+let d3 = document.querySelector(".d3");
+
+const roll = document.querySelector(".roll");
+roll.addEventListener('click', rollDice);
+
+function rollDice(d1, d2, d3) {
+    let n1 = Math.floor((Math.random() * 6) + 1);
+    let n2 = Math.floor((Math.random() * 6) + 1);
+    let n3 = Math.floor((Math.random() * 6) + 1);
+
+
+}
+
