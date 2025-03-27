380584cf6df86dd48af6e86313d6353b40bc700c
Will VanderWyden <vanderwyden@gmail.com>
2025-03-27 13:43:47 +0900

    button styling

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,6 +62,11 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Do not cache assets in development
+  config.assets.debug = true
+  config.assets.check_precompiled_asset = false
+  config.assets.compile = true
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
